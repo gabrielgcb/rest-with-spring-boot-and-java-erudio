c7c05e2d2f9b130506a42bf3205e4ca3d86f6dff
Gabriel Bispo <gabriel.bispo@discente.ufg.br>
2023-09-12 09:49:51 -0300

    feat: migrations

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,9 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Ap. Goiânia-GO', 'Lucas', 'Male', 'Bispo'),
+	(4, 'Brooklin-NY', 'Mary', 'Female', 'Jane'),
+	(5, 'Brooklin-NY', 'Peter', 'Male', 'Parker'),
+	(6, 'Manhattan-NY', 'Harry', 'Male', 'Osborn'),
+	(8, 'Brooklin-NY', 'Ben', 'Male', 'Parker'),
+	(9, 'Queens-NY', 'Ben', 'Male', 'Parker v2'),
+	(10, 'Queens-NY', 'Ben', 'Male', 'Parker v1');
+	
